a4c90f52a55ef7be955af7a3aa414248168441ea
zhaozhiming <kingzzm1982@sina.com>
2015-09-17 13:55:33 +0800

    insert task

--- simple-todos.js
+++ simple-todos.js
@@ -3,7 +3,21 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      event.preventDefault();
+      var text = event.target.taskText.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      event.target.taskText.value = "";
     }
   });
 }
