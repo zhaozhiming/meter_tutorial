4908617e4d94cc7674c1cc945decade47f28dc98
zhaozhiming <kingzzm1982@sina.com>
2015-09-17 11:26:44 +0800

    use mongo db to store data

--- simple-todos.js
+++ simple-todos.js
@@ -1,13 +1,10 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+Tasks = new Mongo.Collection("tasks");
 
+if (Meteor.isClient) {
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1"},
-      { text: "This is task 2"},
-      { text: "This is task 3"}
-    ]
+    tasks: function () {
+      return Tasks.find({});
+    }
   });
 }
 
